067300b1a1e8716b28145db6fccc4634cbe4ea4e
AhmedFalahALharbi <ahmedfalahalharbi@gmail.com>
2024-09-08 12:20:43 +0300

    create file

---
+++ main.js
@@ -0,0 +1,38 @@
+// Q1
+let recX = 10;
+let recY = 20;
+let rectangle = recX * recY;
+console.log(rectangle);
+//  Q2 
+let cdegree = 40;
+let fdegree = (cdegree * 9/5) + 32;
+console.log(fdegree);
+let fdegreeTocdegree = (fdegree  - 32 ) * 5/9 ;
+console.log(fdegreeTocdegree);
+//  Q3
+let hours = 3
+let seconds = 60 * 60 * hours
+console.log(`${hours} hours = ${seconds} Seconds`);
+
+//  Q4
+let thingOne = "foot";
+let thingTwo = "ball";
+console.log(thingOne.concat(thingTwo));
+// Q5
+let bill = 10.25 + 3.99 + 7.15
+let tip  = bill * 0.15
+console.log(bill + tip);
+// Q6
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+console.log(`The Intro to JavaScript course is ${adjective1}. James and Julia are so ${adjective2}. I cannot wait to work through the rest of this ${adjective3} content!`);
+// Q7
+var name = "Ahmed";
+var interest = "Tea";
+var hobby = "football";
+console.log(`my name is ${name}. I love ${interest}. In my spare time, I like to ${hobby}.`);
+
+
+
+
